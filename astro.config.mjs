6713e4539f5ac37331245ba8e2fb5f692875b155
Hugo de Oliveira <hugo@triangulo.dev>
2022-10-27 09:33:19 -0300

    Add OG tags and information on Layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://triangulo.dev/',
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
