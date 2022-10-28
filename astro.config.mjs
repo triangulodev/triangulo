c5e2d765bd2712f17d39a004022cd33526333334
Hugo de Oliveira <hugo@triangulo.dev>
2022-10-28 09:32:55 -0300

    Add remarkToc integration for ToCs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,25 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import image from "@astrojs/image";
 
+import mdx from "@astrojs/mdx";
+import remarkToc from "remark-toc";
+import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://triangulo.dev/',
-  integrations: [mdx(), image(), sitemap()],
+  site: "https://triangulo.dev/",
+  integrations: [
+    mdx({
+      remarkPlugins: [[remarkToc, { heading: "Índice", tight: true }]],
+    }),
+    image(),
+    sitemap(),
+  ],
   markdown: {
     shikiConfig: {
-      theme: "css-variables"
+      theme: "css-variables",
     },
-    extendDefaultPlugins: true
-  }
+    extendDefaultPlugins: true,
+  },
 });
+
