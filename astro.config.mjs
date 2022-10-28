d58ef6f864879a11aed0b74a1a9b46579663b09e
Hugo de Oliveira <hugo@triangulo.dev>
2022-10-28 09:13:23 -0300

    Integrate with @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://triangulo.dev/',
-  integrations: [mdx(), image()],
+  integrations: [mdx(), image(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "css-variables"
