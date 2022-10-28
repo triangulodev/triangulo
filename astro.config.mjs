d9d32c83b78fa1babf3d2e85ba38eb6fe5a0f645
Hugo de Oliveira <hugo@triangulo.dev>
2022-10-27 22:23:10 -0300

    Configure @astrojs/image to improve image support in pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://triangulo.dev/',
-  integrations: [mdx()],
+  integrations: [mdx(), image()],
   markdown: {
     shikiConfig: {
-      theme: "css-variables",
+      theme: "css-variables"
     },
-    extendDefaultPlugins: true,
-  },
+    extendDefaultPlugins: true
+  }
 });
